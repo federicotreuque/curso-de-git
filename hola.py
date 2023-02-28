4fd00a770acd4b0ac5a2c6228a851a2048eed76d
fedetreuque <80726233+federicotreuque@users.noreply.github.com>
2023-02-28 11:37:27 -0300

    modificacion2 hola.py
    faltaba una modificacion

--- hola.py
+++ hola.py
@@ -1,4 +1,4 @@
-#un alumno es profesor, 1 alumno es asistente
+#un alumno es profesor, un alumno es asistente
 #pedir la edad de los compas y ordenarlos de menor a mayor.
 #el mayor de la clase es el profe y el menor el asistente
 
